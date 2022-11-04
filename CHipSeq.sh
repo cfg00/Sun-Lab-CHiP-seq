a69ef5a8c468f4eba59bd1beff1c6c05e3c8352e
cfg00 <cfg001@ucsd.edu>
2022-11-04 13:12:35 -0700

    Added bwa aligmnent command (first part of pipeline)

--- CHipSeq.sh
+++ CHipSeq.sh
@@ -23,13 +23,15 @@ trim=0
 
 rsd=$(pwd) #directory where RNA seq commands are stored, may not be root of fastq reads
 
+#bash <file.sh> -f <folder where fastq files are> -o <folder where you want output> -t <number of threads>
+#-r read length, matters if illumina seq/ alignment,-p paired reads or not (mostly everything is paired),
 for var in "$@"
 do
   if [[ $fqFlag == 1 ]]
 	then
 		fqFlag=0
 		fqDir="$var"
-		quantify=2
+		#quantify=2
 	elif [[ $oFlag == 1 ]]
 	then
 		oFlag=0
@@ -44,35 +46,47 @@ do
 		threads=$var
 	elif [[ "$var" == "-"* ]]
 	then
+		if [[ "$var" == *"-a"* ]]
+		then	
+			align=1 
+		fi
+
 		if [[ "$var" == *"-f"* ]]
 		then
 			fqFlag=1
 		fi
+
 		if [[ "$var" == *"-o"* ]]
 		then
 			oFlag=1
 		fi
+
 		if [[ "$var" == *"-r"* ]]
 		then
 			rFlag=1
 		fi
+
 		if [[ "$var" == *"-t"* ]]
 		then
 			threadFlag=1
 		fi
+
 		if [[ "$var" == *"-p"* ]]
 		then
 			pFlag=1
 			paired=1
 		fi
+
 		if [[ "$var" == "-Q"* ]]
 		then
 			qReport=1
 		fi
+		
 		if [[ "$var" == "-T"* ]]
 		then
 			trim=1
 		fi
+
 	elif [[ $pFlag == 1 ]]
 	then
 		pFlag=2
@@ -162,3 +176,65 @@ then
 		mv $filename "${filename%_clean.fq}.fq"
 	done
 fi
+
+if [[ $align != 0 ]] 
+then
+#cd "$fqDir"
+	#for prefix in Oct4 Klf4 Sox2 H3K27ac H3K4me2 input
+	#do
+    #bwa mem -t 6 /datasets/cs185-sp22-a00-public/genomes/GRCm38.fa /datasets/cs185-sp22-a00-public/lab4/${prefix}.esc.fastq | samtools view -bS > ${prefix}.bam
+	#done
+
+
+	#my colleague jordan wrote most of this.
+	if ! [ -d refGen ] #checks if it a folder named this exits
+	then
+		mkdir refGen
+	fi
+	cd refGen
+	if ! [ -f GCF_000001405.40_GRCh38.p14_genomic.fna ]
+	then
+		if ! [ -f GCF_000001405.40_GRCh38.p14_genomic.fna.gz ]
+		then
+			wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.40_GRCh38.p14/GCF_000001405.40_GRCh38.p14_genomic.fna.gz
+		fi
+		gzip -d GCF_000001405.40_GRCh38.p14_genomic.fna.gz
+		#use mv command to change to .fa
+	fi
+#	if ! [ -f GCF_000001405.40_GRCh38.p14_genomic.gtf ] might need this, check later
+#	then
+#		if ! [ -f GCF_000001405.40_GRCh38.p14_genomic.gtf.gz ]
+#		then
+#			wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.40_GRCh38.p14/GCF_000001405.40_GRCh38.p14_genomic.gtf.gz
+#		fi
+#		gzip -d GCF_000001405.40_GRCh38.p14_genomic.gtf.gz
+#	fi
+
+	cd ../
+
+	cd $fqDir
+
+	mkdir ${oDir%/}
+	chmod -R 0777 ${oDir%/}
+	mkdir "${oDir%/}/Alignment"
+	chmod -R 0777 "${oDir%/}/Alignment" #no crash
+	for filename in *${pair1}.fq 
+	do
+
+		if [ -f "${filename%${pair1}.fq}${pair2}.fq" ]
+		then
+		base =  "${filename%${pair1}.fq}"
+
+		read1 = $filename
+		read2 = "${filename%${pair1}.fq}${pair2}.fq"
+
+		bwa mem -t $threads ${rsd}/refGen/GCF_000001405.40_GRCh38.p14_genomic.fna $read1 $read2 | samtools view -bS > ${base}.bam
+		
+
+    #bwa mem -t 6 /datasets/cs185-sp22-a00-public/genomes/GRCm38.fa /datasets/cs185-sp22-a00-public/lab4/${prefix}.esc.fastq | samtools view -bS > ${prefix}.bam
+	done
+	## next step is to add sequence
+
+
+fi 
+
